e08b875b0205d60cb3883388cc018e468f5df76f
pradeep <pradeep.misra@gmail.com>
2022-03-10 11:28:15 +0530

    Implementation to show or hide the shopping cart

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import store from './components/store/index';
+import store from './store/index';
 
 ReactDOM.render(
     <Provider store={store}>
